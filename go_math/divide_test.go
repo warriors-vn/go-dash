1a1b5e9e6b7352936c62b5d34af361abe9366540
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-08-02 22:28:59 +0700

    feat: adding func divide

---
+++ go_math/divide_test.go
@@ -0,0 +1,20 @@
+package go_math
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_divide_valid(t *testing.T) {
+	result := divide(float64(6), float64(4))
+
+	assert.Equal(t, 1.5, result)
+}
+
+func Test_divide_divisor_zero(t *testing.T) {
+	result := divide(float64(6), float64(0))
+
+	assert.Equal(t, math.Inf(1), result)
+}
