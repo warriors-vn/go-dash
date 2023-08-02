1acfe43ae0f139ac2432c6a868bdca9550518746
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-08-02 14:24:53 +0700

    chore: updating comment func ends with

--- string/ends_with.go
+++ string/ends_with.go
@@ -2,6 +2,10 @@ package string
 
 import "go-dash/constants"
 
+// endsWith checks if the string 'str' ends with the target string 'target' optionally
+// up to a specified 'position' in 'str'.
+// It returns true if 'str' ends with 'target', and false otherwise.
+// If 'position' is provided, only the characters up to that position in 'str' are considered.
 func endsWith(str, target string, position ...int) (bool, error) {
 	pos := len(str)
 	if position != nil {
