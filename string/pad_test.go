37bbe493701898a250577736fb369ab1cc54307e
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-08-02 17:44:07 +0700

    feat: adding func pad

---
+++ string/pad_test.go
@@ -0,0 +1,37 @@
+package string
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_pad_valid_no_char(t *testing.T) {
+	result := pad("abc", 8)
+	assert.Equal(t, "  abc   ", result)
+}
+
+func Test_pad_valid_one(t *testing.T) {
+	result := pad("abc", 8, "_-")
+	assert.Equal(t, "_-abc_-_", result)
+}
+
+func Test_pad_valid_two(t *testing.T) {
+	result := pad("Hello", 6, "*")
+	assert.Equal(t, "Hello*", result)
+}
+
+func Test_pad_valid_three(t *testing.T) {
+	result := pad("Hello", 10, "*")
+	assert.Equal(t, "**Hello***", result)
+}
+
+func Test_pad_invalid_length(t *testing.T) {
+	result := pad("abc", 2)
+	assert.Equal(t, "abc", result)
+}
+
+func Test_pad_char_empty(t *testing.T) {
+	result := pad("abc", 6, "")
+	assert.Equal(t, "abc", result)
+}
