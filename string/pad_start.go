3fb26037d2d8f87eb0b1caf19a61ec3e50021428
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-08-02 18:03:50 +0700

    feat: adding func pad start

---
+++ string/pad_start.go
@@ -0,0 +1,28 @@
+package string
+
+// padStart pads a string 's' with a specified 'length' by adding 'char' at the beginning.
+// If 'char' is not provided, it defaults to a space character.
+// The resulting padded string will have a total length of at least 'length'.
+// If the original string length is greater than or equal to 'length', the original string is returned unchanged.
+func padStart(s string, length int, char ...string) string {
+	if len(s) >= length || (char != nil && char[0] == "") {
+		return s
+	}
+
+	paddingChar := " "
+	if char != nil {
+		paddingChar = char[0]
+	}
+
+	padLen, leftPadding, cur := length-len(s), "", 0
+	for index := 0; index < padLen; index++ {
+		leftPadding += string(paddingChar[cur])
+
+		cur++
+		if cur >= len(paddingChar) {
+			cur = 0
+		}
+	}
+
+	return leftPadding + s
+}
