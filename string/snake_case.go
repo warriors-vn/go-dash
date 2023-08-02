41a3f6755fa8f58c034eebbd342bc3623fd5b7a2
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-08-02 19:07:56 +0700

    feat: adding func snake case

---
+++ string/snake_case.go
@@ -0,0 +1,26 @@
+package string
+
+import "unicode"
+
+// snakeCase converts a string to snake_case format.
+// It replaces spaces and punctuation with underscores and converts letters to lowercase.
+func snakeCase(s string) string {
+	nextToSnakeCase, result := false, ""
+
+	for _, char := range s {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9') {
+			if nextToSnakeCase {
+				result += "_" + string(unicode.ToLower(char))
+				nextToSnakeCase = false
+			} else {
+				result += string(unicode.ToLower(char))
+			}
+		} else {
+			if len(result) > 0 {
+				nextToSnakeCase = true
+			}
+		}
+	}
+
+	return result
+}
