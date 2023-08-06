4c549a71dcf1f5c5f7511f69468f27914e6a94ef
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-08-06 17:54:39 +0700

    feat: add the func filter

---
+++ collection/filter.go
@@ -0,0 +1,43 @@
+package collection
+
+import (
+	"reflect"
+
+	"github.com/warriors-vn/go-dash/constants"
+)
+
+// filter returns a new slice containing elements from the input array that satisfy the predicate.
+// It takes an array-like data structure and a predicate function that determines whether an element should be included.
+// The predicate function should have the signature func(elementType) bool.
+// The function returns the new filtered slice and an error if any occurs.
+func filter(array interface{}, predicate interface{}) (interface{}, error) {
+	arrValue, predicateValue := reflect.ValueOf(array), reflect.ValueOf(predicate)
+
+	if arrValue.Kind() != reflect.Slice && arrValue.Kind() != reflect.Array {
+		return nil, constants.ErrNotSlice
+	}
+
+	if predicateValue.Kind() != reflect.Func {
+		return nil, constants.ErrNotFunction
+	}
+
+	numParams := predicateValue.Type().NumIn()
+	if numParams != 1 {
+		return nil, constants.ErrNotSupport
+	}
+
+	kind, result := predicateValue.Type().In(0).Kind(), reflect.MakeSlice(arrValue.Type(), 0, 0)
+	for i := 0; i < arrValue.Len(); i++ {
+		element := arrValue.Index(i)
+		if element.Kind() != kind {
+			return nil, constants.ErrIncompatible
+		}
+
+		res := predicateValue.Call([]reflect.Value{reflect.ValueOf(element.Interface())})
+		if len(res) > 0 && res[0].Kind() == reflect.Bool && res[0].Interface().(bool) {
+			result = reflect.Append(result, element)
+		}
+	}
+
+	return result.Interface(), nil
+}
