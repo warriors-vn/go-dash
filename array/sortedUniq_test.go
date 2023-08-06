c6cb3034e3bd2cf4f41be2d178db32eca9c8e92a
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-08-06 11:29:59 +0700

    feat: add the func sortedUniq

---
+++ array/sortedUniq_test.go
@@ -0,0 +1,36 @@
+package array
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/warriors-vn/go-dash/constants"
+)
+
+func Test_sortedUniq_valid_int(t *testing.T) {
+	result, err := sortedUniq([]int{1, 1, 2, 3, 4, 4, 5})
+
+	assert.Equal(t, []int{1, 2, 3, 4, 5}, result)
+	assert.Nil(t, err)
+}
+
+func Test_sortedUniq_valid_int64(t *testing.T) {
+	result, err := sortedUniq([]int64{1, 1, 2, 3, 4, 4, 5, 1, 2})
+
+	assert.Equal(t, []int64{1, 2, 3, 4, 5}, result)
+	assert.Nil(t, err)
+}
+
+func Test_sortedUniq_valid_string(t *testing.T) {
+	result, err := sortedUniq([]string{"1", "1", "2", "3", "4", "4", "5"})
+
+	assert.Equal(t, []string{"1", "2", "3", "4", "5"}, result)
+	assert.Nil(t, err)
+}
+
+func Test_sortedUniq_invalid_array_not_slice(t *testing.T) {
+	result, err := sortedUniq(true)
+
+	assert.Equal(t, nil, result)
+	assert.Equal(t, constants.ErrNotSlice, err)
+}
