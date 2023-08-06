e52d961f2b450a2f2173dcdc8384ea75a6db910a
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-08-06 13:26:16 +0700

    feat: add the func takeRight

---
+++ array/takeRight.go
@@ -0,0 +1,39 @@
+package array
+
+import (
+	"reflect"
+
+	"github.com/warriors-vn/go-dash/constants"
+)
+
+// takeRight returns a new slice containing the last 'number' elements from the input array.
+// It takes an array-like data structure and an optional number of elements to take.
+// If 'number' is not provided or is greater than the length of the array, all elements are taken.
+// The function returns the new slice and an error if any occurs.
+func takeRight(array interface{}, number ...int) (interface{}, error) {
+	arrValue := reflect.ValueOf(array)
+
+	if arrValue.Kind() != reflect.Slice && arrValue.Kind() != reflect.Array {
+		return nil, constants.ErrNotSlice
+	}
+
+	if arrValue.Len() == 0 {
+		return array, nil
+	}
+
+	n := 1
+	if number != nil && number[0] >= 0 {
+		n = number[0]
+	}
+
+	if n >= arrValue.Len() {
+		n = arrValue.Len()
+	}
+
+	result := reflect.MakeSlice(arrValue.Type(), 0, 0)
+	for i := arrValue.Len() - n; i < arrValue.Len(); i++ {
+		result = reflect.Append(result, arrValue.Index(i))
+	}
+
+	return result.Interface(), nil
+}
