faa77f5bca819fd13288d6629593ef4bb0902be2
tuannguyen <nguyenvantuan2391996@gmail.com>
2023-08-06 21:22:04 +0700

    feat: add the func sample

---
+++ collection/sample_test.go
@@ -0,0 +1,33 @@
+package collection
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/warriors-vn/go-dash/constants"
+)
+
+func Test_sample_valid_int(t *testing.T) {
+	result, err := sample([]int{1, 2, 3})
+
+	assert.Nil(t, err)
+	if result == 1 || result == 2 || result == 3 {
+		assert.Nil(t, nil)
+	}
+}
+
+func Test_sample_valid_string(t *testing.T) {
+	result, err := sample([]string{"1", "2", "3"})
+
+	assert.Nil(t, err)
+	if result == "1" || result == "2" || result == "3" {
+		assert.Nil(t, nil)
+	}
+}
+
+func Test_sample_invalid_array_not_slice(t *testing.T) {
+	result, err := sample(true)
+
+	assert.Equal(t, nil, result)
+	assert.Equal(t, constants.ErrNotSlice, err)
+}
